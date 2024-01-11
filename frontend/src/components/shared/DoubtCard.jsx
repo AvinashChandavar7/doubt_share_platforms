2f43ba17597143b1f8f1c8b1ec2ca0a553023c96
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-01-11 18:49:17 +0530

    feat: add backend render link

--- frontend/src/components/shared/DoubtCard.jsx
+++ frontend/src/components/shared/DoubtCard.jsx
@@ -56,3 +56,8 @@ const DoubtCard = ({ doubt }) => {
 }
 
 export default DoubtCard
+
+
+
+
+
--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,3 +1,4 @@
 export const config = {
-  endpoint: `http://localhost:8000/api/v1`,
+  // endpoint: `http://localhost:8000/api/v1`,
+  endpoint: `https://doubt-share-platforms.onrender.com/api/v1`,
 };
